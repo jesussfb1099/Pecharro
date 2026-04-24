408201bf555704e7feba7d046b9f46f7dd1bc0b3
Jesus Felipe Barquilla <jesus@192.168.5.241>
2026-04-24 13:06:59 +0200

    Fix server apunta a public

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const rooms = {};
 
